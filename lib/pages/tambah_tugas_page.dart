c75fd68f5e5a7de397a58626a23a4fb74033ad6e
devitazaqi <devitazaqi@gmail.com>
2023-12-07 19:16:07 +0700

    menambahkan validator _formKey

--- lib/pages/tambah_tugas_page.dart
+++ lib/pages/tambah_tugas_page.dart
@@ -6,6 +6,7 @@ class TambahTugasPage extends StatelessWidget {
 
   TambahTugasPage({super.key, required this.onSubmit});
 
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _judulController = TextEditingController();
   final TextEditingController _deskripsiController = TextEditingController();
 
@@ -17,29 +18,46 @@ class TambahTugasPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: _judulController,
-              decoration: const InputDecoration(labelText: 'Judul'),
-            ),
-            TextField(
-              controller: _deskripsiController,
-              decoration: const InputDecoration(labelText: 'Deskripsi'),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                Tugas tugas = Tugas(
-                    judul: _judulController.text,
-                    deskripsi: _deskripsiController.text);
-                onSubmit(tugas);
-                Navigator.pop(context);
-              },
-              child: const Text('Simpan'),
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _judulController,
+                decoration: const InputDecoration(labelText: 'Judul'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "judul harus diisi";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _deskripsiController,
+                decoration: const InputDecoration(labelText: 'Deskripsi'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "deskripsi harus diisi";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Tugas tugas = Tugas(
+                        judul: _judulController.text,
+                        deskripsi: _deskripsiController.text);
+                    onSubmit(tugas);
+                    Navigator.pop(context);
+                  }
+                },
+                child: const Text('Simpan'),
+              ),
+            ],
+          ),
         ),
       ),
     );
